bc16303bf759aea30513bcba4ddde9d15407aef6
AverageFrancis <142629365+AverageFrancis@users.noreply.github.com>
2024-10-11 09:34:21 -0300

    a

---
+++ perguntas.js
@@ -0,0 +1,130 @@
+criaCartao(
+    'Matematica',
+    'quanto é 1 + 1',
+    '2'
+    //1//
+)
+
+criaCartao(
+    'Biologia',
+    'Quem descobriu a evolução',
+    'Charles Darwin'
+    //2//
+)
+criaCartao(
+    'Biologia',
+    'Quem descobriu a hereditariedade ',
+    'Mendel'
+    //3//
+)
+
+criaCartao(
+    'Historia',
+    'Quem matou hitler',
+    'Ele se matou'
+    //4//
+)
+
+criaCartao(
+    'Português',
+    'Mais se refere a quantidade ou a porem',
+    'quantidade'
+    //5//
+)
+criaCartao(
+    'Ingles',
+    'Oque significa cock',
+    'galo'
+    //6//
+)
+
+criaCartao(
+    'Biotecnologia',
+    'Quais os tipos sanguineos',
+    'A,B,AB e O'
+    //7//
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital de Angola',
+    'Luanda'
+    //8//
+)
+
+criaCartao(
+    'historia',
+    'Quem foi Dom Pedro II',
+    'Rei do Brasil'
+    //9//
+)
+
+criaCartao(
+    'Matematica',
+    'quanto é 0 elevado a 0',
+    'indefinido'
+    //10//
+) 
+criaCartao(
+    'Geografia',
+    'Qual a capital do Egito',
+    'Cairo'
+    //11//
+)
+
+criaCartao(
+    'Sociologia',
+    'Qual a causa do machismo',
+    'homens'
+    //12//
+)
+criaCartao(
+    'Sociologia',
+    'Quem foi Karl Marx',
+    'Ele foi based e redpill'
+    //13//
+)
+
+criaCartao(
+    'Biologia',
+    'Quanto é um 1 ao quadrado',
+    '1'
+    //14//
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Estônia',
+    'Talin'
+    //15//
+) 
+criaCartao(
+    'Matematica',
+    'Quem descobriu o teorema de pitagoras',
+    'Pitagoras'
+    //16//
+)
+
+criaCartao(
+    'Historia',
+    'Freddy Mercury morreu de que',
+    'Aids'
+    //17//
+)
+criaCartao(
+    'Quimica',
+    'O que é benzeno',
+    'uma cadeia de carbono fechada com ligações duplas intercaladas'
+    //18//
+)
+
+criaCartao(
+    'Quimica',
+    'O que é fenol',
+    'Um composto com uma hidroxila ligada a um anel aromatico'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o país com a maior fronteira terrestre com o Brasil',
+    'França'
+)
